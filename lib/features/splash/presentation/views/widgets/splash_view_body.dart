ccee2d436014fde26661d50c30b0057d99aea10b
ElManDoh <ashraftrezegeh@gmail.com>
2024-12-03 23:56:05 +0200

    add home view and custom app bar

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/animated_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -17,13 +20,8 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
-            .animate(animationController);
-
-    animationController.forward();
+    initSlidingAnimationText();
+    navigateToHome();
   }
 
   @override
@@ -41,5 +39,25 @@ class _SplashViewBodyState extends State<SplashViewBody>
           Image.asset(AssetsApp.logo),
           AnimatedText(slidingAnimation: slidingAnimation)
         ]);
+  }
+
+  void initSlidingAnimationText() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        Get.to(const HomeView(),
+            transition: Transition.fade, duration: kTransitionDuration);
+      },
+    );
   }
 }
