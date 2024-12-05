d403a53b1b1d1b3186f3a8a2198832a611f50cdc
ElManDoh <ashraftrezegeh@gmail.com>
2024-12-05 20:10:06 +0200

    best seller list view item done

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
@@ -8,7 +10,7 @@ class BestSellerListViewItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 130,
+      height: 120,
       child: Row(
         children: [
           AspectRatio(
@@ -27,21 +29,75 @@ class BestSellerListViewItem extends StatelessWidget {
           const SizedBox(
             width: 30,
           ),
-           Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: const Text(
-                  "Harry Potter and the Globet of Fire",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .5,
+                  child: Text(
+                    "Harry Potter and the Globet of Fire",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: Styles.textStyle20.copyWith(fontFamily: kGTSectraFine),
+                  ),
                 ),
-              )
-            ],
+                const SizedBox(
+                  height: 3,
+                ),
+                const Text(
+                  'J.K. Rowling',
+                  style: Styles.textStyle14,
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '19.99 €',
+                      style: Styles.textStyle20.copyWith(
+                        fontWeight: FontWeight.bold,
+                        // color: Colors.white
+                      ),
+                    ),const BookingRating()
+                  ],
+                ),
+              ],
+            ),
           )
         ],
       ),
+    );
+  }
+}
+
+class BookingRating extends StatelessWidget {
+  const BookingRating({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Icon(FontAwesomeIcons.solidStar,color: Color(0xffFFDD4F),),
+        const SizedBox(
+          width: 6.3,
+        ),
+        const Text(
+          "4.8",
+          style: Styles.textStyle16,
+        ),
+        const SizedBox(
+          width: 5,
+        ),
+        Text(
+          "(2450)",
+          style: Styles.textStyle14.copyWith(
+            color: const Color(0xff707070),
+          ),
+        )
+      ],
     );
   }
 }
