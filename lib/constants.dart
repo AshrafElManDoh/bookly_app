e4f68b8e7ae68c11ccc8984d85004f45ee4596db
ElManDoh <ashraftrezegeh@gmail.com>
2024-12-09 15:54:02 +0200

    refactor navigation

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 const kScaffoldBackgroundColor = Color(0xff100B20);
-const kTransitionDuration = Duration(milliseconds: 250);
+// const kTransitionDuration = Duration(milliseconds: 250);
 const kGTSectraFine = "GT Sectra Fine";
