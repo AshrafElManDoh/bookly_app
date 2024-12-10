328fbbb26e4769af4239db5179cc39a6f96ffac2
ElManDoh <ashraftrezegeh@gmail.com>
2024-12-10 18:57:07 +0200

    add custom book item to book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -5,28 +7,21 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          CustomBookdetailsAppBar(),
+          const CustomBookdetailsAppBar(),
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.18),
+            child: const CustomBookItem(),
+          )
         ],
       ),
-    );
-  }
-}
-
-class CustomBookdetailsAppBar extends StatelessWidget {
-  const CustomBookdetailsAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        IconButton(onPressed: (){}, icon: const Icon(Icons.close),),
-        IconButton(onPressed: (){}, icon: const Icon(Icons.shopping_cart_outlined),),
-      ],
     );
   }
 }
